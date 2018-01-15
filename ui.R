d984bce9a05822ea2e2aa0d36086e46cdc3f1b03
Sasha Bogdanovic <sasha@rubyind.com>
2018-01-15 02:14:08 -0800

    BCHDV-20 Remove superfluos main navigation links

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ ui <- navbarPage(
   theme = "css/bcgov.css",
   title = "British Columbia Housing Market Data Visualization",
   source(file.path("ui", "home.R"),  local = TRUE)$value,
-  source(file.path("ui", "ptt.R"),  local = TRUE)$value,
   source(file.path("ui", "census.R"),  local = TRUE)$value,
+  source(file.path("ui", "ptt.R"),  local = TRUE)$value,
   source(file.path("ui", "census_search.R"),  local = TRUE)$value
 )
