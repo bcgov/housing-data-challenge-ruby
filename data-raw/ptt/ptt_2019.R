a3cc7f35b3e802af4822bff4337ad68836601052
Sasha Bogdanovic <sasha@rubyind.com>
2019-08-27 20:39:47 -0700

    BCHDV-99 Add 2019 data from Open Data Catalogue - DR and dashboard

--- data-raw/ptt/ptt_2019.R
+++ data-raw/ptt/ptt_2019.R
@@ -87,24 +87,117 @@ ptt_dr_2019 <- ptt_dr_bcdc %>% bchousing::WranglePttData(calculate_percentages =
 ptt_rd_2019 <- ptt_rd_bcdc %>% bchousing::WranglePttData(calculate_percentages = FALSE, region_name_split = TRUE)
 ptt_mn_2019 <- ptt_mn_bcdc %>% bchousing::WranglePttData(calculate_percentages = FALSE, region_name_split = TRUE)
 
-# 04. Add geometries to PTT data ----
-ptt_dr_sf_2019 <- bchousing::JoinPttShapes(
-  ptt_data = ptt_dr_2019,
-  shapes = shapes_dr,
-  geo_name = "DevelopmentRegion"
-)
+c <- as_tibble(colnames(ptt_dr_2019))
 
-ptt_rd_sf_2019 <- bchousing::JoinPttShapes(
-  ptt_data = ptt_rd_2019,
-  shapes = shapes_rd,
-  geo_name = "RegionalDistrict")
+FixPastData <- function(data) {
+  data <- data %>%
+    select(-c(GeoUID, PRUID, PRNAME)) %>%
+    rename(DevelopmentRegion = GeoName) %>%
+    mutate(
+      n_tot_corp_tran = as.numeric(NA),
+      n_tot_indv_tran = as.numeric(NA),
+      n_res_strata_rental = as.numeric(NA),
+      n_res_unkn = as.numeric(NA),
+      n_comm_industry = as.numeric(NA),
+      n_comm_util = as.numeric(NA),
+      n_comm_unkn = as.numeric(NA),
+      n_forest_tran = as.numeric(NA),
+      n_foreign_comm = as.numeric(NA),
+      n_foreign_oth_unk = as.numeric(NA),
+      n_foreign_corp_tran = as.numeric(NA),
+      n_foreign_indv_tran = as.numeric(NA),
+      perc_n_foreign_res = as.numeric(NA),
+      perc_FMV_foreign_res = as.numeric(NA),
+      sum_invest_foreign_res = as.numeric(NA),
+      perc_invest_foreign_res = as.numeric(NA),
+      n_lt1M_foreign_res = as.numeric(NA),
+      n_gt1M_foreign_res = as.numeric(NA),
+      n_gt3M_foreign_res = as.numeric(NA)
+    )
+  st_geometry(data) <- NULL
+  return(data)
+}
 
-ptt_mn_sf_2019 <- bchousing::JoinPttShapes(
-  ptt_data = ptt_mn_2019,
-  shapes = shapes_mun,
-  geo_name = "Municipality"
-)
+FixCurrentData <- function(data) {
+  data <- data %>%
+    rename(
+      'no_res_trans' = 'n_res_trans',
+      'n_res_1fam_dwelling' = 'n_res_1fam',
 
+    ) %>%
+    mutate(
+      n_res_other = n_res_strata_rental + n_res_unkn,
+      n_comm_other = n_comm_industry + n_comm_util + n_comm_unkn,
+      n_foreign_nonres = n_foreign_comm + n_foreign_oth_unk,
+      n_res_1fam_suite = as.numeric(NA),
+      n_res_1fam_vacant_res = as.numeric(NA),
+      n_res_1fam_other = as.numeric(NA),
+      n_res_strata_row = as.numeric(NA),
+      n_res_strata_other = as.numeric(NA),
+      no_foreign_perc = 0,
+      sum_FMV_foreign_perc = 0
+    )
+  return(data)
+}
+
+# Fix past data
+ptt_dr_past <- ptt_dr_sf %>%
+  select(-c(GeoUID, PRUID, PRNAME)) %>%
+  rename(DevelopmentRegion = GeoName) %>%
+  mutate(
+    n_tot_corp_tran = as.numeric(NA),
+    n_tot_indv_tran = as.numeric(NA),
+    n_res_strata_rental = as.numeric(NA),
+    n_res_unkn = as.numeric(NA),
+    n_comm_industry = as.numeric(NA),
+    n_comm_util = as.numeric(NA),
+    n_comm_unkn = as.numeric(NA),
+    n_forest_tran = as.numeric(NA),
+    n_foreign_comm = as.numeric(NA),
+    n_foreign_oth_unk = as.numeric(NA),
+    n_foreign_corp_tran = as.numeric(NA),
+    n_foreign_indv_tran = as.numeric(NA),
+    perc_n_foreign_res = as.numeric(NA),
+    perc_FMV_foreign_res = as.numeric(NA),
+    sum_invest_foreign_res = as.numeric(NA),
+    perc_invest_foreign_res = as.numeric(NA),
+    n_lt1M_foreign_res = as.numeric(NA),
+    n_gt1M_foreign_res = as.numeric(NA),
+    n_gt3M_foreign_res = as.numeric(NA)
+  )
+st_geometry(ptt_dr_past) <- NULL
+
+
+ptt_dr_2019 <- ptt_dr_2019 %>%
+  rename(
+    'no_res_trans' = 'n_res_trans',
+    'n_res_1fam_dwelling' = 'n_res_1fam',
+
+  ) %>%
+  mutate(
+    n_res_other = n_res_strata_rental + n_res_unkn,
+    n_comm_other = n_comm_industry + n_comm_util + n_comm_unkn,
+    n_foreign_nonres = n_foreign_comm + n_foreign_oth_unk,
+    n_res_1fam_suite = as.numeric(NA),
+    n_res_1fam_vacant_res = as.numeric(NA),
+    n_res_1fam_other = as.numeric(NA),
+    n_res_strata_row = as.numeric(NA),
+    n_res_strata_other = as.numeric(NA),
+    no_foreign_perc = 0,
+    sum_FMV_foreign_perc = 0
+  )
+
+ptt_dr_past <- FixPastData(ptt_dr_sf)
+ptt_rd_past <- FixPastData(ptt_rd_sf)
+ptt_mn_past <- FixPastData(ptt_mun_sf)
+
+ptt_dr_2019 <- FixCurrentData(ptt_dr_2019)
+ptt_rd_2019 <- FixCurrentData(ptt_rd_2019)
+ptt_mn_2019 <- FixCurrentData(ptt_mn_2019)
+
+ptt_dr_j <- full_join(ptt_dr_past, ptt_dr_2019)
+ptt_rd_j <- full_join(ptt_rd_past, ptt_rd_2019)
+ptt_mn_j <- full_join(ptt_mn_past, ptt_mn_2019)
 
 # Examine values of Municipality name and shape Municipality for records that match and can be joined
 # Shapes, not PTT
@@ -129,46 +222,69 @@ ptt_mn_sf_2019 <- bchousing::JoinPttShapes(
 
 # 05. Set up dashboard data ----
 # Last month for which PTT data is available
-max_trans_period <- max(ptt_pr$trans_period)
+max_trans_period <- max(ptt_pr_2019$trans_period)
 
-ptt_pr_dash <- ptt_pr %>%
+ptt_prov_dash <- ptt_pr_2019 %>%
   filter(trans_period == max_trans_period) %>%
   select(tot_mkt_trans, perc_n_foreign_res, sum_FMV, perc_FMV_foreign_res) %>%
-  mutate(max_trans_period = max_trans_period) # %>%
-  # rename(
-  #   no_foreign_perc = perc_n_foreign_res,
-  #   sum_FMV_foreign_perc = perc_FMV_foreign_res
-  # )
-
-# 05. Join 2019 data to previous data ----
-ptt_dr_past <- ptt_dr_sf
-st_geometry(ptt_dr_past) <- NULL
-st_geometry(ptt_dr_sf_2019) <- NULL
-
-ptt_dr_sf_2019 <- ptt_dr_sf_2019 %>%
-  rename(
-    'no_res_trans' = 'n_res_trans',
-    'n_res_1fam' = 'n_res_1fam_dwelling',
-
-  ) %>%
   mutate(
-    n_res_other = n_res_strata_rental + n_res_unkn,
-    n_comm_other = n_comm_industry + n_comm_util + n_comm_unkn,
-    n_foreign_nonres = n_foreign_comm + n_foreign_oth_unk
+    max_trans_period = max_trans_period,
+    perc_n_foreign_res = perc_n_foreign_res * 100,
+    perc_FMV_foreign_res = perc_FMV_foreign_res * 100
+  ) %>%
+  rename(
+    no_foreign_perc = perc_n_foreign_res,
+    sum_FMV_foreign_perc = perc_FMV_foreign_res
   )
-
-ptt_dr_j <- full_join(ptt_dr_past, ptt_dr_sf_2019)
 
 
 # 06. Add geometries back ----
-ptt_dr_sf_2019 <- JoinPttShapes(ptt_data = ptt_dr, shapes = shapes_dr, geo_name = "DevelopmentRegion")
-ptt_rd_sf_2019 <- JoinPttShapes(ptt_data = ptt_rd, shapes = shapes_rd, geo_name = "RegionalDistrict")
-ptt_mn_sf_2019 <- JoinPttShapes(ptt_data = ptt_mn, shapes = shapes_mun, geo_name = "Municipality")
+ptt_dr_sf <- JoinPttShapes(
+    ptt_data = ptt_dr_j,
+    shapes = shapes_dr,
+    geo_name = "DevelopmentRegion"
+  # ) %>%
+  # mutate_if(
+  #   is.nan, list(~na_if(., NA))
+  ) %>%
+  mutate(
+    GeoUID = forcats::as_factor(GeoUID),
+    GeoName = forcats::as_factor(GeoName),
+    PRUID = forcats::as_factor(PRUID),
+    PRNAME = forcats::as_factor(PRNAME)
+  )
+
+ptt_rd_sf <- JoinPttShapes(
+    ptt_data = ptt_rd_j,
+    shapes = shapes_rd,
+    geo_name = "RegionalDistrict"
+  ) %>%
+  mutate(
+    GeoUID = forcats::as_factor(GeoUID),
+    GeoName = forcats::as_factor(GeoName),
+    PRUID = forcats::as_factor(PRUID),
+    PRNAME = forcats::as_factor(PRNAME)
+  )
+
+ptt_mun_sf <- JoinPttShapes(
+    ptt_data = ptt_mn_j,
+    shapes = shapes_mun,
+    geo_name = "Municipality"
+  ) %>%
+  mutate(
+    GeoUID = forcats::as_factor(GeoUID),
+    GeoName = forcats::as_factor(GeoName),
+    PRUID = forcats::as_factor(PRUID),
+    PRNAME = forcats::as_factor(PRNAME)
+  )
+
+# ptt_rd_sf_2019 <- JoinPttShapes(ptt_data = ptt_rd_j, shapes = shapes_rd, geo_name = "RegionalDistrict")
+# ptt_mn_sf_2019 <- JoinPttShapes(ptt_data = ptt_mn_j, shapes = shapes_mun, geo_name = "Municipality")
 
 
 # 07. Save .rda files into data directory ----
-usethis::use_data(ptt_dr_sf, overwrite = TRUE, compress = "gzip")
-usethis::use_data(ptt_rd_sf, overwrite = TRUE, compress = "gzip")
-usethis::use_data(ptt_mun_sf, overwrite = TRUE, compress = "gzip")
+# usethis::use_data(ptt_dr_sf, overwrite = TRUE, compress = "gzip")
+# usethis::use_data(ptt_rd_sf, overwrite = TRUE, compress = "gzip")
+# usethis::use_data(ptt_mun_sf, overwrite = TRUE, compress = "gzip")
 usethis::use_data(ptt_prov_dash, overwrite = TRUE)
 
